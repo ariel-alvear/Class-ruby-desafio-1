ca58776135af56580f158c473de0dbac039e83cf
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-25 09:41:16 -0300

    desafio cartas listos!

--- carta.rb
+++ carta.rb
@@ -1,33 +1,18 @@
 class Card
-    attr_accessor :number, :pinta
+    attr_accessor :number, :pinta, :cards_n
     def initialize
         @number = [1,2,3,4,5,6,7,8,9,10,11,12,13].sample
         @pinta = ['C', 'D', 'E', 'T'].sample
     end
-end
-
-class Card_deck
-    attr_accessor :name, :cards
-    def initialize(name, cards=nil)
-        @name = name
-        @cards = []
-        @cards.push cards
+    def insert_5_cards
+        @cards_n = Array.new
+        5.times do |i|
+            @cards_n.push(Card.new)
+        end
     end
 end
 
+
 card1 = Card.new
-card2 = Card.new
-card3 = Card.new
-card4 = Card.new
-card5 = Card.new
-
-card_deck1 = Card_deck.new('first_card_deck')
-
-card_deck1.cards.push card1
-card_deck1.cards.push card2
-card_deck1.cards.push card3
-card_deck1.cards.push card4
-card_deck1.cards.push card5
-
-puts card_deck1.name
-puts card_deck1.cards
+puts card1.insert_5_cards
+puts card1.cards_n
